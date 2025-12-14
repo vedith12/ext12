26a904c437ae3fcb728aa94694ae00192a09227e
Rasagna2320 <rasagna.k6@gmail.com>
2025-12-14 07:09:11 +0530

    19Q

---
+++ 19Q/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const logger = require("./middleware/logger");
+
+
+app.use(logger);
+app.use(express.static("public"));// Frontend
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
